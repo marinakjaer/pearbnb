40f807274e9887e1aeaa950947399e0865688c67
Alan Keegan <alan.c.keegan@gmail.com>
2018-11-26 16:21:20 +0100

    redundant routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookings/create'
-  get 'bookings/new'
-  get 'bookings/destroy'
-  get 'users/create'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
-  get 'flats/create'
-  get 'flats/new'
-  get 'flats/edit'
-  get 'flats/show'
-  get 'flats/update'
-  get 'flats/destroy'
-  get 'flats/results'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flats, except: [:index]
