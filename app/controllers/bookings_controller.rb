fffdba6b4351fae4c2f9d4eb3fc4f84cc46499c7
Alan Keegan <alan.c.keegan@gmail.com>
2018-11-27 15:21:51 +0100

    buttons and path for booking creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,31 @@
 class BookingsController < ApplicationController
   def create
+    @flat = set_flat
+    @booking = Booking.new(booking_params)
+    @booking.flat = @flat
+    @booking.user = current_user
+    @booking.status = false
+    if @booking.save
+      redirect_to flat_path(@flat)
+    else
+      render:new
+    end
   end
 
   def new
+    @flat = set_flat
+    @booking = Booking.new
   end
 
   def destroy
+  end
+
+  private
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :user, :flat, :check_out)
   end
 end
