86f94e8bc9c5b0b13a049452a426d252c634ac4a
Alan Keegan <alan.c.keegan@gmail.com>
2018-11-28 13:13:10 +0100

    presence and uniquness validations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,11 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
   mount_uploaders :photos, PhotoUploader
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :photos, presence: true
+  validates :description, presence: true
+  validates :price_per_night, presence: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: true
   has_many :bookings, dependent: :destroy
   has_many :flats, dependent: :destroy
 end
